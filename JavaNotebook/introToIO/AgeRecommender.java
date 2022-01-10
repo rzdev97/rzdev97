98e78faddcc6ead84ed0b8ef2d9efdab74931bd2
Rene Zorzi <31076613+rzdev97@users.noreply.github.com>
2022-01-10 09:31:39 +0100

    Update and rename creatingAMinimumAgeRecommender to AgeRecommender.java

---
+++ JavaNotebook/introToIO/AgeRecommender.java
@@ -0,0 +1,19 @@
+public class AgeRecommender{
+
+
+public class static void main (String[] args){
+System.out.println("Age Recommender SetUp");
+
+//request country and age from user in system
+
+//request for type of object for age verification
+
+
+//check for input matching with array stored
+
+
+//output text with age expected, and if it is already possible
+
+
+}
+}
